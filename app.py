a0ff1f477eb2900c499b374089e466fc2f070868
balazske96 <venyigebalazs96@gmail.com>
2019-04-03 13:49:22 +0200

    Cool welcome pages

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/game')
 def tilitoli():
     return render_template('index.html')
+
+@app.route('/', methods=['POST', 'GET'])
+def welcome():
+    if request.method == 'GET':
+        return render_template('welcomepage.html')
+    else:
+        player_name = request.form['name']
+        return render_template('welcomepage.html', player=player_name)
 
 
 if __name__ == '__main__':
